d9a3eb2482fece2b5e984b0b9ecd9742c0b18f69
Hyperborean-8 <85061131+Hyperborean-8@users.noreply.github.com>
2023-11-18 23:52:11 +0300

    time frame is almost done

---
+++ examples/tutorial/entry.py
@@ -0,0 +1,15 @@
+from tkinter import ttk
+import tkinter as tk
+
+def validate_entry(text):
+    return text.isdecimal()
+
+root = tk.Tk()
+root.config(width=300, height=200)
+root.title("My App")
+entry = ttk.Entry(
+    validate="key",
+    validatecommand=(root.register(validate_entry), "%S")
+)
+entry.place(x=50, y=50, width=150)
+root.mainloop()
--- main.py
+++ main.py
@@ -10,9 +10,14 @@ class SelectTimeFrame(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
+        self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=0)
         self.grid_columnconfigure(1, weight=1)
-        # Заранее выбранные опции
+
+        # --- Слайдер и надписи ---
+        self.slider_frame = ctk.CTkFrame(self)
+        self.slider_frame.grid(row=0, column=0, sticky='news')
+
         self.time_options = [('4 часа', 14400, 0),
                              ('2 часа', 7200, 1),
                              ('1 час', 3600, 2),
@@ -20,25 +25,58 @@ class SelectTimeFrame(ctk.CTkFrame):
                              ('10 минут', 600, 4),
                              ('0 минут', 0, 5)]
 
-        self.SetTimeSlider = ctk.CTkSlider(self, from_=0, to=1, number_of_steps=5, orientation='vertical')
-        self.SetTimeSlider.grid(row=0, column=0, padx=10, pady=20, sticky="news")
-        self.label_list: list = []
+        self.time_slider = ctk.CTkSlider(self.slider_frame, from_=0, to=1, number_of_steps=5, orientation='vertical')
+        self.time_slider.grid(row=0, column=0, padx=10, pady=20, sticky="news")
+        self.time_slider.set(0)
 
-        self.label_frame = ctk.CTkFrame(self)
+        self.label_list: list = []
+        self.label_frame = ctk.CTkFrame(self.slider_frame)
         self.label_frame.grid(row=0, column=1, padx=10, pady=10, sticky='news')
+
         for txt, val, pos in self.time_options:
-            # Кнопки
             label = ctk.CTkLabel(self.label_frame, text=txt)
             label.grid(row=pos, column=1, sticky="nesw")
             self.label_frame.rowconfigure(pos, weight=1)
             self.label_list.append(label)
 
+        # --- Выбор времени ---
+        self.time_frame = ctk.CTkFrame(self)
+        self.time_frame.grid(row=0, column=1, sticky="news")
 
-class CurrentTimeFrame(ctk.CTkFrame):
+        self.time_inner_frame = ctk.CTkFrame(self.time_frame)
+        self.time_inner_frame.grid(row=1, column=1, sticky="news")
+
+        self.time_frame.grid_rowconfigure(0, weight=1)
+        self.time_frame.grid_rowconfigure(1, weight=0)
+        self.time_frame.grid_rowconfigure(2, weight=1)
+        self.time_frame.grid_columnconfigure(0, weight=1)
+        self.time_frame.grid_columnconfigure(1, weight=0)
+        self.time_frame.grid_columnconfigure(2, weight=1)
+
+        self.font = ctk.CTkFont(size=80)
+        self.time_entry = ctk.CTkEntry(self.time_inner_frame, font=self.font, width=105, height=80, border_width=0,
+                                       corner_radius=0, fg_color='black')
+        self.current_input = ''
+        self.time_entry.grid(row=0, column=0)
+        self.time_type = ctk.CTkSegmentedButton(self.time_inner_frame, values=['Минут', 'Часов'])
+        self.time_type.grid(row=1, column=0)
+        self.time_type.set('Минут')
+
+    def validate_entry(self):  # Todo: Сделать валидацию
+        current_input = self.current_input
+        new_input = self.time_entry.get()
+
+        if not new_input.isdigit():
+            self.time_entry.delete(0)
+            self.time_entry.insert(0, current_input)
+        elif new_input.isdigit():
+            self
+
+
+class CurrentTimeFrame(ctk.CTkFrame):  # Todo: Сделать фрейм с текущем временем
 
     def __init__(self, master):
         super().__init__(master)
-
 
 
 class ButtonsFrame(ctk.CTkFrame):
@@ -59,7 +97,7 @@ class ButtonsFrame(ctk.CTkFrame):
 
 
 class App(ctk.CTk):
-    def __init__(self, bg_color = 'transparent'):
+    def __init__(self,):
         super().__init__()
 
         self.minimized_rows: bool = False
@@ -68,12 +106,13 @@ class App(ctk.CTk):
         self.time_set: int = 0
         self.time: int = 0
 
-        # # Прикрепление сигнала изменения к переменной
-        # self.bind("<Configure>", self.handle_configure)
+        # Прикрепление сигнала изменения к переменной
+        self.bind("<Key>", self.handle_configure)
 
         # Настройка окна
         self.title("Timesleep")
-        self.geometry("300x380")
+        self.geometry("300x315")
+        self.resizable(False, False)
         # self.minsize(130, 200)
 
         # Настройка сетки
@@ -94,7 +133,7 @@ class App(ctk.CTk):
         # Это прикол который нам поможет потом))
         # self.attributes('-topmost', True)
 
-    def set_time(self, time):
+    def set_time(self, time):  # Todo: Сделать систему времени
         print(time)
 
     def start_time(self, time):
@@ -105,6 +144,10 @@ class App(ctk.CTk):
 
     def stop_time(self):
         self.time
+
+    def handle_configure(self, event):  # Todo: Сделать валидацию
+        print('что-то поменялось ', event)
+        self.SelectTimeFrame.validate_entry()
 
 app = App()
 app.mainloop()
