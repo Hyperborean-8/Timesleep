ee912d9eed89339e49d2059952be7e85718f21de
Hyperborean-8 <85061131+Hyperborean-8@users.noreply.github.com>
2023-11-30 09:21:45 +0300

    Добавил окно с настройками и подтверждения

--- main.py
+++ main.py
@@ -4,12 +4,22 @@ import math as math
 from PIL import Image
 from icecream import ic
 import os
+from configparser import ConfigParser
 
 ctk.set_appearance_mode("dark")  # Режимы: системный (стандартный), светлый, тёмный
 ctk.set_default_color_theme("blue")  # Темы: синяя (стандартная), тёмно-синяя, зелёная
-ctk.deactivate_automatic_dpi_awareness()
-debug = True
+ctk.deactivate_automatic_dpi_awareness()  # Программа больше не реагирует на изменение интерфейса ОС.
+debug = True  # Режим отладки. Не позволяет программе выключить компьютер.
+
 # ic.disable()
+
+# Настройки
+parser = ConfigParser()
+parser.read('settings.ini')
+
+confirmation = bool(parser.get('confirmation', 'confirm_when_timer_on'))
+ic(confirmation)
+
 
 def hide_window():
     app.withdraw()
@@ -20,9 +30,61 @@ def show_window():
 
 
 def turn_off_computer():
-
     if not debug:
         os.system("shutdown /s /t 1")
+
+
+class ConfirmationWindow(ctk.CTkToplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("Timesleep")
+        self.geometry("450x200")
+
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=0)
+        self.rowconfigure(2, weight=1)
+
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=0)
+        self.columnconfigure(2, weight=1)
+
+        self.LabelFrame = ctk.CTkFrame(self)
+        self.LabelFrame.grid(row=1, column=1)
+
+        self.label = ctk.CTkLabel(self.LabelFrame, text='Вы уверены что хотите выйти? Таймер всё ещё работает.')
+        self.label.grid(row=0, column=0)
+
+        self.checkbox = ctk.CTkCheckBox(self.LabelFrame, text='Больше не спрашивать', font=('Arial', 12),
+                                        checkbox_height=16, checkbox_width=16, border_width=2)
+        self.checkbox.grid(row=2, column=0, padx=5, pady=5)
+
+        self.ButtonsFrame = ctk.CTkFrame(self, height=20)
+        self.ButtonsFrame.grid(row=3, column=1, sticky='ews', pady=(0,15))
+
+        self.quit_button = ctk.CTkButton(self.ButtonsFrame)
+        self.quit_button.grid(row=0, column=0)
+        self.ButtonsFrame.columnconfigure(1, weight=1)
+        self.cancel_button = ctk.CTkButton(self.ButtonsFrame)
+        self.cancel_button.grid(row=0, column=2)
+
+
+
+# Окно с настройками
+class SettingsWindow(ctk.CTkToplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("Настройки")
+        self.geometry("400x300")
+
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+
+        self.ScrollableFrame = ctk.CTkScrollableFrame(self)
+        self.ScrollableFrame.grid(row=0, column=0, pady=10, padx=10, sticky='news')
+
+        self.checkbox = ctk.CTkCheckBox(self.ScrollableFrame, text='Просить подтверждение выхода при запущеном таймере', checkbox_width=16, checkbox_height=16)
+        self.checkbox.grid(row=0, column=0, padx=5, pady=5)
+
 
 # Коробка с выбором времени
 class SelectTimeFrame(ctk.CTkFrame):
@@ -66,13 +128,15 @@ class SelectTimeFrame(ctk.CTkFrame):
             self.LabelFrame.rowconfigure(pos, weight=1)
             self.label_list.append(label)
 
-        # --- Выбор времени ---
+        # Основная коробка
         self.TimeFrame = ctk.CTkFrame(self)
         self.TimeFrame.grid(row=0, column=1, sticky="news")
 
+        # Внутренняя коробка, которая будет оставаться в центре основной
         self.TimeInnerFrame = ctk.CTkFrame(self.TimeFrame)
         self.TimeInnerFrame.grid(row=1, column=1, sticky="news")
 
+        # Центрирование внутренней коробки
         self.TimeFrame.grid_rowconfigure(0, weight=1)
         self.TimeFrame.grid_rowconfigure(1, weight=0)
         self.TimeFrame.grid_rowconfigure(2, weight=1)
@@ -80,16 +144,22 @@ class SelectTimeFrame(ctk.CTkFrame):
         self.TimeFrame.grid_columnconfigure(1, weight=0)
         self.TimeFrame.grid_columnconfigure(2, weight=1)
 
-        self.font = ctk.CTkFont(size=80)
+        # Энтри для вписывание времени
+        self.font = ctk.CTkFont(size=80)  # Шрифт
         self.time_entry = ctk.CTkEntry(self.TimeInnerFrame, font=self.font, width=105, height=80, border_width=0,
                                        corner_radius=0, fg_color='#101010')
-        self.current_input = ''
         self.time_entry.grid(row=0, column=0)
+
+        # Выбор типа времени
         self.time_type = ctk.CTkSegmentedButton(self.TimeInnerFrame, values=['Минут', 'Часов'],
                                                 command=self.change_by_entry)
         self.time_type.grid(row=1, column=0)
         self.time_type.set('Минут')
 
+        # Переменные
+        self.current_input = ''
+
+    # Валидация вписанного текста (текст должен быть числом, без минуса или букв)
     def validate_entry(self):
         new_input = self.time_entry.get()
 
@@ -104,7 +174,6 @@ class SelectTimeFrame(ctk.CTkFrame):
 
         # Если entry длиннее чем 2
         if len(new_input) > 2:
-
             # Удалить третий символ
             self.time_entry.delete(2)
 
@@ -112,6 +181,7 @@ class SelectTimeFrame(ctk.CTkFrame):
         if not self.master.timer_on:
             self.change_by_entry()
 
+    # Функция, вызываемая при изменении слайдера
     def change_by_slider(self, x):
         time_options = copy.deepcopy(self.time_options)
         time_options.reverse()
@@ -124,6 +194,7 @@ class SelectTimeFrame(ctk.CTkFrame):
         for y in range(len(self.time_type.get()), -1, -1):
             self.time_entry.delete(y)
 
+        # Автоматически менять время на часы/минуты, если время слишком большое/маленькое
         if seconds <= 5940:
 
             self.time_type.set('Минут')
@@ -134,8 +205,10 @@ class SelectTimeFrame(ctk.CTkFrame):
             self.time_type.set('Часов')
             self.time_entry.insert(string=str(math.ceil(seconds / 3600)), index=0)
 
+        # Установить время в app()
         self.master.set_timer(seconds)
 
+    # Функция, вызываемая если пользователь что-то вписал в энтри
     def change_by_entry(self, *_x):
         # Todo: Сделать чтобы энтри изменял значение слайдера (сделать после создания кастомных слайдеров)
         time_type = self.time_type.get()
@@ -156,14 +229,17 @@ class SelectTimeFrame(ctk.CTkFrame):
 
         print('[00] В entry вписано:', new_time, time_type, 'или', seconds, 'секунд')
 
+        # Установить время в app()
         self.master.set_timer(seconds)
 
 
+# Коробка, показывающая таймер
 class CurrentTimeFrame(ctk.CTkFrame):
 
     def __init__(self, master):
         super().__init__(master)
 
+        # Центрирование
         self.grid_rowconfigure(0, weight=1)
         self.grid_rowconfigure(1, weight=0)
         self.grid_rowconfigure(2, weight=0)
@@ -202,8 +278,9 @@ class CurrentTimeFrame(ctk.CTkFrame):
         minutes = seconds // 60
         seconds = seconds % 60
 
+        # Добавляет 0 перед числом, чтобы таймер выглядит как 01:02:50, а не как 1:2:50
+        # Не используется, когда остаются только секунды
         def tr(text):
-            text = str(text)
             text = str(text)
 
             if len(text) < 2:
@@ -217,11 +294,13 @@ class CurrentTimeFrame(ctk.CTkFrame):
         elif hours == 0 and minutes == 0:
             label_text = f'{seconds}'
         else:
-            label_text = 'Something went wrong'
+            label_text = 'Что-то пошло не так'
 
+        # Меняет текст на полученный
         self.time_label.configure(text=label_text)
 
 
+# Фрейм с кнопками
 class ButtonsFrame(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master, fg_color="transparent")
@@ -231,13 +310,12 @@ class ButtonsFrame(ctk.CTkFrame):
 
         image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "icons")
         ic(image_path)
-        self.icon_settings_light = ctk.CTkImage(Image.open(os.path.join(image_path, "icon-settings-light.png")), size=(20, 20))
+        self.icon_settings_light = ctk.CTkImage(Image.open(os.path.join(image_path, "icon-settings-light.png")),
+                                                size=(20, 20))
         self.icon_minimize_light = ctk.CTkImage(Image.open(os.path.join(image_path, "icon-minimize-light.png")),
                                                 size=(20, 20))
 
-
-
-        # Кнопки
+        # Кнопки "Старт" и "Стоп"
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
 
@@ -247,16 +325,27 @@ class ButtonsFrame(ctk.CTkFrame):
         self.stop_button.grid(row=0, column=1, padx=10, pady=10, sticky='ew')
         self.stop_button.grid_remove()
 
-        self.ExtraButtonFrame = ctk.CTkFrame(self, height=30, width=150)
+        # Дополнительные кнопки
+        self.ExtraButtonFrame = ctk.CTkFrame(self, height=30, width=150, fg_color='transparent')
         self.ExtraButtonFrame.grid(row=0, column=0, sticky='ew', padx=10, pady=10)
 
         self.ExtraButtonFrame.columnconfigure(0, weight=0)
         self.ExtraButtonFrame.columnconfigure(1, weight=1)
 
-        self.settings_button = ctk.CTkButton(self.ExtraButtonFrame, text='', image=self.icon_settings_light, width=20, height=20)
+        self.settings_button = ctk.CTkButton(self.ExtraButtonFrame, text='', image=self.icon_settings_light,
+                                             width=20, height=20, command=self.open_settings)
         self.settings_button.grid(row=0, column=0)
-        self.minimize = ctk.CTkButton(self.ExtraButtonFrame, text='', image=self.icon_minimize_light, width=20, height=20)
+
+        self.minimize = ctk.CTkButton(self.ExtraButtonFrame, text='', image=self.icon_minimize_light,
+                                      width=20, height=20)
         self.minimize.grid(row=0, column=2)
+
+    def open_settings(self):
+        if self.master.settings is None or not self.master.settings.winfo_exists():
+            self.master.settings = SettingsWindow(self)  # create window if its None or destroyed
+            self.after(10, self.master.settings.focus)
+        else:
+            self.master.settings.focus()  # if window exists focus it
 
     def hide_start(self):
         self.start_button.grid_remove()
@@ -267,10 +356,12 @@ class ButtonsFrame(ctk.CTkFrame):
         self.stop_button.grid_remove()
 
 
+# Основной класс для программы
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
 
+        # Переменные для таймера
         self.timer_after = None
         self.minimized_rows: bool = False
         self.options_hidden: bool = False
@@ -279,8 +370,14 @@ class App(ctk.CTk):
         self.seconds_set: int = 0
         self.seconds: int = 0
 
-        # Прикрепление сигнала изменения к переменной
+        # Переменные для окон
+        self.SettingsWindow = None
+        self.ConfirmationWindow = None
+
+        # Прикрепление сигнала Клавиш к переменной
         self.bind("<Key>", self.handle_configure)
+
+        self.protocol('WM_DELETE_WINDOW', self.display_confirmation)
 
         # Настройка окна
         self.title("Timesleep")
@@ -293,7 +390,7 @@ class App(ctk.CTk):
         self.grid_rowconfigure(0, weight=1)
         self.grid_rowconfigure(1, weight=0)
 
-        # Frames
+        # Коробки
         self.SelectTimeFrame = SelectTimeFrame(self)
         self.SelectTimeFrame.grid(row=0, column=0, padx=10, pady=(15, 0), sticky="news")
         self.CurrentTimeFrame = CurrentTimeFrame(self)
@@ -302,12 +399,10 @@ class App(ctk.CTk):
         self.ButtonsFrame = ButtonsFrame(self)
         self.ButtonsFrame.grid(row=1, column=0, padx=10, pady=5, sticky='news')
 
-        # self.SelectTimeFrame.grid_remove()
-        # self.CurrentTimeFrame.grid() # TODO: -----------------------------------------------------------------
-
         # Это прикол который нам поможет потом))
         # self.attributes('-topmost', True)
 
+    # Ставит время таймера
     def set_timer(self, seconds):
         print('[03] Время установлено на: ', seconds)
 
@@ -318,6 +413,7 @@ class App(ctk.CTk):
         else:
             self.ButtonsFrame.start_button.configure(state=ctk.NORMAL)
 
+    # Запускает таймер с выставленными секундами
     def start_timer(self):
         print('[04] Время запущено с', self.seconds_set, 'секундами.')
 
@@ -333,6 +429,7 @@ class App(ctk.CTk):
 
         self.update_timer()
 
+    # Останавливает таймер
     def stop_timer(self):
         self.timer_on = False
 
@@ -343,10 +440,7 @@ class App(ctk.CTk):
 
         self.after_cancel(self.timer_after)
 
-    def handle_configure(self, _event):
-
-        self.SelectTimeFrame.validate_entry()
-
+    # Функция, которая вызывает саму себя и обновляет время каждую секунду
     def update_timer(self):
 
         if self.extra_second:
@@ -367,6 +461,21 @@ class App(ctk.CTk):
         else:
             pass
 
+    # Функция, вызываемая при нажатии клавиш
+    def handle_configure(self, _event):
 
+        # Валидация
+        self.SelectTimeFrame.validate_entry()
+
+    def display_confirmation(self):
+        if self.ConfirmationWindow is None or not self.ConfirmationWindow.winfo_exists():
+            self.ConfirmationWindow = ConfirmationWindow(self)  # create window if its None or destroyed
+            self.after(80, self.ConfirmationWindow.focus)
+        else:
+            self.ConfirmationWindow.focus()  # if window exists focus it
+
+
+# Главные циклы
 app = App()
+app.display_confirmation()
 app.mainloop()
